7249318350ab91ffe2a615ae83229387926b460c
Taranjot Singh Bindra <taran16501@gmail.com>
2025-10-09 17:14:46 -0400

    Delete chat.py

--- chat.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from backend_app.services.text_engine import generate_response
-
-router = APIRouter()
-
-@router.post("/")
-def chat_endpoint(user_input: str):
-    bot_reply = generate_response(user_input)
-    return {"user_input": user_input, "bot_response": bot_reply}
